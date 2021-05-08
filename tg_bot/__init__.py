8c8bde2f796861c4f4d0e6ef06295c5f4c14aa85
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-08 14:01:38 +0500

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -36,7 +36,7 @@ if ENV:
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
-     MESSAGE_DUMP = os.environ.get('MESSAGE_DUMP', None)
+     MESSAGE_DUMP = os.environ.get("MESSAGE_DUMP", None)
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
 
     try:
